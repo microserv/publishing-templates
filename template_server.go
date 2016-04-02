ff9e225b2ca9da71307efd9b2d0bcf7faae7ac7e
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-04-02 13:18:31 +0200

    Create the skeleton for a basic router which will be used to get, add, update, and delete templates stored in our database.

--- template_server.go
+++ template_server.go
@@ -1,13 +1,52 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type Template struct {
+	id   int64  "db:'id' json:'id'"
+	name string "db:'name' json:'template_name'"
+	html string "db:'html' json:'html'"
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and server on 0.0.0.0:8080
+	version := "1"
+	var router *gin.Engine
+	router = CreateRouter(version)
+	router.Run(":8080")
+}
+
+func CreateRouter(version string) *gin.Engine {
+	router := gin.Default()
+
+	api_group := router.Group(fmt.Sprintf("api/v%s", version))
+	{
+		api_group.GET("/template/", GetAllTemplates)
+		api_group.GET("/template/:template_name", GetTemplate)
+
+		api_group.POST("/template", AddTemplate)
+		api_group.PUT("/template/:template_name", UpdateTemplate)
+
+		api_group.DELETE("/template/:template_name", DeleteTemplate)
+	}
+
+	return router
+}
+
+func GetTemplate(c *gin.Context) {
+	c.JSON(404, "ads")
+}
+
+func GetAllTemplates(c *gin.Context) {
+}
+
+func AddTemplate(c *gin.Context) {
+}
+
+func UpdateTemplate(c *gin.Context) {
+}
+
+func DeleteTemplate(c *gin.Context) {
 }
