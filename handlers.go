4e106423a0776a7519673f0887139a6e5bffb7a7
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-04-02 14:09:49 +0200

    Separate the functions of the server into two files.     -  'server.go' handles all web server related activities, such as        initilizing routes and managing the server. The routes were places in        this file, as it felt overkill to separate them into a dedicated file        when this project is so small.     -  'handlers.go' contains the requests handlers, ex. the function which is        to process requests to '/template/'.

---
+++ handlers.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetTemplate(c *gin.Context) {
+	c.JSON(404, "ads")
+}
+
+func GetAllTemplates(c *gin.Context) {
+}
+
+func AddTemplate(c *gin.Context) {
+}
+
+func UpdateTemplate(c *gin.Context) {
+}
+
+func DeleteTemplate(c *gin.Context) {
+}
