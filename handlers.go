fcb813051d5b78a0c5ae9e365e34ba05a4e3dd69
sklirg <hlsolbjorg@gmail.com>
2016-04-05 01:05:16 +0200

    Tabs to spaces

--- handlers.go
+++ handlers.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"strconv"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "strconv"
 )
-
-// The format for loglines
-const LOGFORMAT = "[%v] %v - %v | %v | %v | %v"
 
 func GetTemplate(c *gin.Context) {
     templates := getTemplatesByName(c.Param("template_name"))
@@ -19,27 +16,27 @@ func GetTemplate(c *gin.Context) {
 }
 
 func GetAllTemplates(c *gin.Context) {
-	var _num = c.DefaultQuery("limit", "10")
-	
-	num, err := strconv.Atoi(_num)
-	if err != nil {
-		str := fmt.Sprintf("Invalid value for \"limit\", should be int but was %T (%v).", _num, _num)
-		c.JSON(400, generateJSONErr(400, str))
-		c.Abort()
-		return
-	}
-	
-	var response [10]map[string]string
-	
-	response = generateDummyTemplates(num)
-	
-	c.JSON(200, response)
+    var _num = c.DefaultQuery("limit", "10")
+    
+    num, err := strconv.Atoi(_num)
+    if err != nil {
+        str := fmt.Sprintf("Invalid value for \"limit\", should be int but was %T (%v).", _num, _num)
+        c.JSON(400, generateJSONErr(400, str))
+        c.Abort()
+        return
+    }
+    
+    var response [10]map[string]string
+    
+    response = generateDummyTemplates(num)
+    
+    c.JSON(200, response)
 }
 
 func AddTemplate(c *gin.Context) {
-	name := c.PostForm("name")
-	template := c.PostForm("template")
-	fmt.Printf("Incoming template: \"%v\"\n%v\n", name, template)
+    name := c.PostForm("name")
+    template := c.PostForm("template")
+    fmt.Printf("Incoming template: \"%v\"\n%v\n", name, template)
 }
 
 func UpdateTemplate(c *gin.Context) {
--- server.go
+++ server.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
+    "fmt"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	version := "1"
+    version := "1"
 
-	engine := gin.Default()
-	AddRoutes(version, engine)
+    engine := gin.Default()
+    AddRoutes(version, engine)
     
     connectToDb()
     
-	engine.Run(":8080")
+    engine.Run(":8080")
 }
 
 func AddRoutes(version string, engine *gin.Engine) {
-	api_group := engine.Group(fmt.Sprintf("api/v%s", version))
-	{
-		api_group.GET("/template/", GetAllTemplates)
-		api_group.GET("/template/:template_name", GetTemplate)
+    api_group := engine.Group(fmt.Sprintf("api/v%s", version))
+    {
+        api_group.GET("/template/", GetAllTemplates)
+        api_group.GET("/template/:template_name", GetTemplate)
 
-		api_group.POST("/template", AddTemplate)
-		api_group.PUT("/template/:template_name", UpdateTemplate)
+        api_group.POST("/template", AddTemplate)
+        api_group.PUT("/template/:template_name", UpdateTemplate)
 
-		api_group.DELETE("/template/:template_name", DeleteTemplate)
-	}
+        api_group.DELETE("/template/:template_name", DeleteTemplate)
+    }
 }
